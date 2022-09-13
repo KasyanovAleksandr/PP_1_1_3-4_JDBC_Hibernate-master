715023af23dbe4f6cf13535240980a979ebcd18a
AeKa <a_kasyanov@rambler.ru>
2022-09-13 20:59:21 +0300

    initial setting Create Override toString

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,14 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getConnection;
+
+
+public class Util {
+
+
+    // реализуйте настройку соеденения с БД
+}
